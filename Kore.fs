2ef43720f7c12c63e22ae78d9f9915c08a4384ce
August-Alm <augustalm@gmail.com>
2023-03-30 14:49:23 +0200

    Humble but clean beginnings.

--- Kore.fs
+++ Kore.fs
@@ -1,8 +1,20 @@
 namespace Parsimonious
 
-module Core =
+module Kore =
 
   type Typ = O | Hom of Typ * Typ | B of Typ
+
+  let rec showTyp (typ : Typ) : string =
+    match typ with
+    | O -> "o"
+    | Hom (a, b) -> $"{go a} -> {go b}"
+    | B a -> $"!{go a}"
+  
+  and private go typ =
+    match typ with
+    | O -> "o"
+    | Hom (a, b) -> $"({go a} -> {go b})"
+    | B a -> $"!{go a}"
 
   /// Terms of the simply typed very parsimonious lambda calculus.
   type Term =
@@ -14,14 +26,29 @@ module Core =
     | Let of string * Term * Term
     | Ann of Term * Typ
   
+  let rec showTerm (trm : Term) =
+    match trm with
+    | Var x -> x
+    | LamAff (x, t) -> $"λ{x}.{showTerm t}" 
+    | LamBox (x, t) -> $"λ!{x}.{showTerm t}"
+    | App (t, u) -> $"({showTerm t} {showTerm u})"
+    | Box t -> $"!{showTerm t}"
+    | Let (x, t, u) -> $"let {x} = {showTerm t}\{showTerm u}"
+    | Ann (t, a) -> $"({showTerm t} : {showTyp a})"
+  
+  /// Values of terms of the simply typed very parsimonious lambda
+  /// calculus. Used in defining semantics and evaluation.
   type Value =
     | VVar of string
-    | VLamAff of string * (Value -> Value)
-    | VLamBox of string * (Value -> Value)
+    | VLamAff of Closure
+    | VLamBox of Closure
     | VApp of Value * Value
     | VBox of Value
   
   and Env = (string * Value) list
+
+  and [<Struct>] Closure =
+    Closure of (string * struct (Env * Term))
 
   let rec lookup (env : Env) x =
     match env with
@@ -36,42 +63,52 @@ module Core =
       | ValueNone -> x
 
   let rec eval env trm =
+    let inline mkClosure x env t = Closure (x, struct (env, t))
+    let inline (@@) (Closure (x, struct (env, t))) u = eval ((x, u) :: env) t
     match trm with
     | Var x -> (lookup env x).Value
-    | LamAff (x, t) -> VLamAff (x, fun u -> eval ((x, u) :: env) t)
-    | LamBox (x, t) -> VLamBox (x, fun u -> eval ((x, u) :: env) t)
+    | LamAff (x, t) -> VLamAff (mkClosure x env t)
+    | LamBox (x, t) -> VLamBox (mkClosure x env t)
     | App (t, u) ->
       match eval env t, eval env u with
-      | VLamAff (_, t), u -> t u
-      | VLamBox (_, t), VBox u -> t u
+      | VLamAff xt, u -> xt @@ u
+      | VLamBox xt, VBox u -> xt @@ u
       | t, u -> VApp (t, u)
     | Box t -> VBox (eval env t)
     | Let (x, t, u) -> eval ((x, eval env t) :: env) u
     | Ann (t, _) -> eval env t
 
+  let inline (@@) (Closure (x, struct (env, t))) u = eval ((x, u) :: env) t
+  
   let rec quote env v =
     match v with
     | VVar x -> Var x
-    | VLamAff (x, t) ->
+    | VLamAff (Closure (x, _) as xt) ->
       let x = fresh env x
-      let xv = VVar x
-      LamAff (x, quote ((x, xv) :: env) (t xv))
-    | VLamBox (x, t) ->
+      let vx = VVar x
+      LamAff (x, quote ((x, vx) :: env) (xt @@ vx))
+    | VLamBox (Closure (x, _) as xt) ->
       let x = fresh env x
-      let xv = VVar x
-      LamBox (x, quote ((x, xv) :: env) (t xv))
+      let vx = VVar x
+      LamBox (x, quote ((x, vx) :: env) (xt @@ vx))
     | VApp (t, u) -> App (quote env t, quote env u)
     | VBox t -> Box (quote env t)
 
+  let showValueWith env v = showTerm (quote env v)
+  
+  let showValue v = showValueWith [] v
+
+  /// Normalization by evaluation, with environment.
   let normalizeWith env t = quote env (eval env t)
 
+  /// Normalization by evaluation.
   let normalize t = normalizeWith [] t
 
   type Usage = Lin of int | Exp of int | Used
   
   type Typing = { Name : string; mutable Usage : Usage; Typ : Typ }
 
-  let rec inline getTyping (ctx : Typing list) x =
+  let rec getTyping (ctx : Typing list) x =
     match ctx with
     | ting :: _ when ting.Name = x -> ValueSome ting 
     | _ :: ctx -> getTyping ctx x
@@ -117,81 +154,6 @@ module Core =
       | Hom (B c, d) ->
         let xc = { Name = x; Usage = Exp dep; Typ = c }
         check (xc :: ctx) dep b d
-      | _ -> failwith $"Box-lambda can't have non-box-function type {typ}."
-    | Box t ->
-      match typ with
-      | B ty -> check ctx (dep + 1) t ty
-      | _ -> failwith $"Type mismatch, box {trm} cannot have non-box type {typ}."
-    | _ ->
-      let inf = infer ctx dep trm
-      if inf <> typ then
-        failwith $"Type mismatch, inferred {inf} instead of {typ}."
-
-
-module Kore =
-
-
-  type Typ = O | Hom of Typ * Typ | B of Typ
-
-  /// Terms of the simply typed very parsimonious lambda calculus.
-  type Term =
-    | Var of string
-    | LamAff of string * (Term -> Term)
-    | LamBox of string * (Term -> Term)
-    | App of Term * Term
-    | Box of Term
-    | Ann of Term * Typ
-  
-  /// Weak head normal form reduction.
-  let rec reduce t =
-    match t with
-    | App (f, a) ->
-      match reduce f, a with
-      | LamAff (_, b), a -> reduce (b a)
-      | LamBox (_, b), Box a -> reduce (b a)
-      | _ -> t
-    | _ -> t
-  
-  type Usage = Lin of int | Exp of int | Used
-  
-  type Typing = { Name : string; mutable Usage : Usage; Typ : Typ }
-
-  /// Bidirectional type inference.
-  let rec infer ctx dep trm =
-    match trm with
-    | Var x ->
-      match List.tryFind (fun ting -> ting.Name = x) ctx with
-      | None -> failwith $"Untyped var {x}."
-      | Some ting ->
-        match ting.Usage with
-        | Lin d when d = dep -> ting.Usage <- Used; ting.Typ
-        | Lin _ -> failwith $"Linear var {x} breaks depth restriction."
-        | Exp d when d = dep -> ting.Typ
-        | Exp d when d = dep - 1 -> ting.Usage <- Used; ting.Typ
-        | Exp _ -> failwith $"Exponential var {x} breaks depth restriction."
-        | Used -> failwith $"Var {x} breaks affine usage rules."
-    | LamAff _ | LamBox _ -> failwith "Can't infer types of raw lambdas."
-    | App (f, a) ->
-      match infer ctx dep f with
-      | Hom (d, c) -> check ctx dep a d; c
-      | _ -> failwith $"Inferred non-function application in {trm}."
-    | Box t -> B (infer ctx (dep + 1) t)
-    | Ann (t, typ) -> check ctx dep t typ; typ
-  
-  /// Bidirectional type checking.
-  and check ctx dep trm typ =
-    match trm with
-    | LamAff (x, b) ->
-      match typ with
-      | Hom (c, d) ->
-        let xc = { Name = x; Usage = Lin dep; Typ = c }
-        check (xc :: ctx) dep (b (Var x)) d
-      | _ -> failwith $"Affine lambda can't have non-function type {typ}."
-    | LamBox (x, b) ->
-      match typ with
-      | Hom (B c, d) ->
-        let xc = { Name = x; Usage = Exp dep; Typ = c }
-        check (xc :: ctx) dep (b (Var x)) d
       | _ -> failwith $"Box-lambda can't have non-box-function type {typ}."
     | Box t ->
       match typ with
--- Program.fs
+++ Program.fs
@@ -5,36 +5,31 @@ module Program =
   open Kore
 
   let two =
-    LamBox ("s", fun s ->
-      LamAff ("z", fun z ->
-        App (s, App (s, z))))
+    LamBox ("s", LamAff ("z",
+        App (Var "s", App (Var "s", Var "z"))))
 
   let Nat a =
     Hom (B (Hom (a, a)), Hom (a, a))
   
   let f =
-    LamBox ("x", fun x ->
-      LamBox ("y", fun y ->
-        Box (App (x, y))))
+    LamBox ("x", LamBox ("y",
+        Box (App (Var "x", Var "y"))))
   
   let T =
     Hom (B (Hom (O, O)), Hom (B O, B O))
   
   let succ =
-    LamAff ("n", fun n ->
-      LamBox ("s", fun s ->
-        LamAff ("z", fun z ->
-          App (s, App (App (n, Box s), z)))))
-  
-
+    LamAff ("n", LamBox ("s", LamAff ("z",
+          App (Var "s", App (App (Var "n", Box (Var "s")), Var "z")))))
 
   [<EntryPoint>]
   let main _ =
+    printfn "nat = %s" (showTyp (Nat O))
     check [] 0 two (Nat O)
     check [] 0 f T
     check [] 0 succ (Hom (Nat O, Nat O))
     let succAnn = Ann (succ, Hom (Nat O, Nat O))
     check [] 0 (App (succAnn, two)) (Nat O)
-    let three = reduce (App (succ, two))
-    printfn "%A" three
+    let three = normalize (App (succ, two))
+    printfn "three = %s" (showTerm three)
     0
